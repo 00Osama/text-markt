88b51231fa7cdc04dfb4a51222c3d19a71849e97
00Osama <osos71117@gmail.com>
2025-07-14 22:12:00 +0300

    add internet connectivity

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lottie/lottie.dart';
 import 'package:textmarkt/auth/services/auth_gate.dart';
 import 'package:textmarkt/bloc/event_cubit.dart';
 import 'package:textmarkt/bloc/note_cubit.dart';
@@ -44,7 +46,37 @@ class _MyAppState extends State<MyApp> {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: const AuthGate(),
+        home: StreamBuilder<ConnectivityResult>(
+          stream: Connectivity().onConnectivityChanged.map(
+              (list) => list.isNotEmpty ? list.last : ConnectivityResult.none),
+          builder: (context, snapshot) {
+            if (snapshot.hasData && snapshot.data != ConnectivityResult.none) {
+              return const AuthGate();
+            } else {
+              return Scaffold(
+                backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Lottie.asset(
+                        'assets/app/noInternet.json',
+                        width: 200,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(height: 60),
+                      Text(
+                        'no internet connection',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            }
+          },
+        ),
         theme: AppTheme.lightTheme(context),
         darkTheme: AppTheme.darkTheme(context),
         themeMode: ThemeMode.system,
